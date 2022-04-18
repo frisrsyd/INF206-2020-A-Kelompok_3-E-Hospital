7acd1f42d764ce508f9df41d43de6ed9354978c6
Faris Rasyid <frisrsyd@gmail.com>
2022-04-18 15:20:00 +0700

    laravel project and create navbar

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #FFEFD9;">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="img/logo.png" height="44" class="d-inline-block align-text-top">
+        </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ms-auto" style="background-color: transparent;" id="fitur_bg">
+            <div class="navbar-nav m-auto align-items-center" id="fitur">
+                <a class="btn btn-grey" href="{{url('#')}}"><img src="img/Notifications.png" alt=""> Notifications</a>
+                <a class="btn bg-gradient-primary" href="{{url('#')}}"><i class="bi bi-clock-history"></i> History</a>
+                <a href="{{url('#')}}" class="btn" style="color: black"><img src="img/on-loan.png" alt=""> On Loan</a>
+                <a href="#" class="btn" style="color: black"><img src="img/account.png" height="33" alt=""></a>
+                
+            </div>
+        </div>
+      </div>
+    </div>
+  </nav>
