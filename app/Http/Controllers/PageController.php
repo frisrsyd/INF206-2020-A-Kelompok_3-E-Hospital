426f326a91662f9ef968401afb11decfda9437fd
Faris Rasyid <frisrsyd@gmail.com>
2022-04-21 00:45:08 +0700

    create landing page

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        return view('page.index');
+    }
+}
