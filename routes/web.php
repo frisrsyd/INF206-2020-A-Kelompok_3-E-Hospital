0f3bec44a2ec2e715c7383f281b9868b13e8d63e
Faris Rasyid <frisrsyd@gmail.com>
2022-04-22 18:15:30 +0700

    adding rekap paminjaman page
    menambahkan halaman rekap peminjaman

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,4 @@ use App\Http\Controllers\PageController;
 
 Route::get('/', [PageController::class, 'index']);
 Route::get('all-tools', [PageController::class, 'allTools']);
+Route::get('rekap-pinjam', [PageController::class, 'rekapPinjam']);
