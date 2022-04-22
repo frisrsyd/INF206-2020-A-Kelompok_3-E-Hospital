d7f50e67851241259b7d5c4e03d0dfddf6e466b5
Faris Rasyid <frisrsyd@gmail.com>
2022-04-23 02:12:11 +0700

    adding input-data page and account page
    membuat halaman input data dan account, tapi halaman account belum fix

---
+++ resources/views/page/account.blade.php
@@ -0,0 +1,34 @@
+@extends('app')
+@section('content')
+    <div class="banner mb-3">
+        <div class="profile-image shadow"></div>
+        <div class="profile-info text-center mt-2">
+            <h4 class="name fw-bold">Fulan bin Fulan</h4>
+            <span class="text-muted">fulan@gmail.com</span>
+        </div>
+        <div class="container ">
+            <div class="row account-container">
+                <div class="col-md-6">
+                    <div class="card account-management">
+                        <div class="card-body">
+                            <table class="table table-bordered">
+                                <tbody class="table-body">
+                                    <tr>
+                                        <td>
+                                            <a href="">Login</a> 
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <a href="{{ url('input-data') }}">Input Data</a> 
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,5 @@ use App\Http\Controllers\PageController;
 Route::get('/', [PageController::class, 'index']);
 Route::get('all-tools', [PageController::class, 'allTools']);
 Route::get('rekap-pinjam', [PageController::class, 'rekapPinjam']);
+Route::get('input-data', [PageController::class, 'inputData']);
+Route::get('account', [PageController::class, 'account']);
