b035222695a62d3cc6a8277d492d4dd6713ebb00
Arif Munandar <arif_mun@mhs.unsyiah.ac.id>
2022-04-25 00:05:29 +0700

    update page ketersediaan barang

---
+++ resources/views/page/ketersediaan.blade.php
@@ -0,0 +1,101 @@
+@extends('app')
+
+@section('content')
+
+<style type="text/css">
+    .element {
+        position: relative;
+        top: 100px;
+    }
+</style>
+
+
+<div class="element">
+<div class="row">
+	<div class="col-sm-6">
+		<h4 class="bg-danger text-white" align="center">RSUD Zainoel Abidin</h4>
+		<div class="card text-center">
+		  <div class="card-header ">
+		    <ul class="nav nav-tabs card-header-tabs">
+		      <li class="nav-item">
+		        <a class="nav-link active" href="#">Info singkat</a>
+		      </li>
+		    </ul>
+		  </div>
+		  <div class="card-body">
+		    <h5 class="card-title">Alamat : Jl. Teuku Moh. Daud Beureueh No.108, Bandar Baru, Kec. Kuta Alam, Kota Banda Aceh, Aceh 24415
+3,0 km</h5>
+		    <p class="card-text">Tersedia : 6 </p>
+		    <a href="#" class="btn btn-primary">Detail</a>
+		  </div>
+		</div>
+	</div>
+</div>
+
+ 
+
+<div class="element">
+	<div class="col-sm-6">
+		<h4 class="bg-danger text-white" align="center">RS TK.II MOH. RIDWAN MEURAKSA</h4>
+		<div class="card text-center">
+		  <div class="card-header">
+		    <ul class="nav nav-tabs card-header-tabs">
+		      <li class="nav-item">
+		        <a class="nav-link active" href="#">Info singkat</a>
+		      </li>
+		    </ul>
+		  </div>
+		  <div class="card-body">
+		    <h5 class="card-title">Alamat : Jl. Taman Mini I No.4, RW.2, Pinang Ranti, Kec. Makasar, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13560</h5>
+		    <p class="card-text">Tersedia : 0 </p>
+		    <a href="#" class="btn btn-primary">Detail</a>
+		  </div>
+		</div>
+	</div>
+</div>
+
+
+<div class="element">
+<div class="row">
+	<div class="col-sm-6">
+		<h4 class="bg-danger text-white" align="center">RS Prince Nayef Unsyiah  </h4>
+		<div class="card text-center">
+		  <div class="card-header ">
+		    <ul class="nav nav-tabs card-header-tabs">
+		      <li class="nav-item">
+		        <a class="nav-link active" href="#">Info singkat</a>
+		      </li>
+		    </ul>
+		  </div>
+		  <div class="card-body">
+		    <h5 class="card-title">Alamat : Jl. Lingkar Kampus, Kopelma Darussalam, Kec. Syiah Kuala, Kota Banda Aceh, Aceh 24411</h5>
+		    <p class="card-text">Tersedia : 2 </p>
+		    <a href="#" class="btn btn-primary">Detail</a>
+		  </div>
+		</div>
+	</div>
+</div>
+
+<div class="element">
+<div class="row">
+	<div class="col-sm-6">
+		<h4 class="bg-danger text-white" align="center">RS Bhayangkara Banda Aceh  </h4>
+		<div class="card text-center">
+		  <div class="card-header ">
+		    <ul class="nav nav-tabs card-header-tabs">
+		      <li class="nav-item">
+		        <a class="nav-link active" href="#">Info singkat</a>
+		      </li>
+		    </ul>
+		  </div>
+		  <div class="card-body">
+		    <h5 class="card-title">Alamat : Jl. Cut Nyak Dhien No.23, Lamtemen Bar., Kec. Jaya Baru, Kota Banda Aceh, Aceh 23232</h5>
+		    <p class="card-text">Tersedia : 1 </p>
+		    <a href="#" class="btn btn-primary">Detail</a>
+		  </div>
+		</div>
+	</div>
+</div>
+</div>
+
+@endsection
