53283df03a005fe894220b67e50662b3fbe77b26
Faris Rasyid <frisrsyd@gmail.com>
2022-05-01 01:12:44 +0700

    adding feature login
    menambahkan fitur login. sekarang user dapat melakukan login sesuai dengan akun yang telah dibuat dan ada pada database

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 
-
+// Page Routes
 Route::get('/', [PageController::class, 'index']);
 Route::get('all-tools', [PageController::class, 'allTools']);
 Route::get('rekap-pinjam', [PageController::class, 'rekapPinjam']);
 Route::get('input-data', [PageController::class, 'inputData']);
 Route::get('account', [PageController::class, 'account']);
-Route::get('signUp', [PageController::class, 'signUp']);
-Route::get('login', [PageController::class, 'login']);
-Route::get('login-admin', [PageController::class, 'loginAdmin']);
 Route::get('lupapassword', [PageController::class, 'lupapassword']);
 Route::get('reset-password', [PageController::class, 'resetPassword']);
 Route::get('ketersediaan-tool', [PageController::class, 'ketersediaanTool']);
@@ -21,4 +19,13 @@ Route::get('detail-history', [PageController::class, 'detailHistory']);
 Route::get('edit-profile', [PageController::class, 'editProfile']);
 Route::get('notifikasi', [PageController::class, 'notifikasi']);
 
+// middleware guest
+Route::get('signUp', [PageController::class, 'signUp'])->middleware('guest');
+Route::get('login', [PageController::class, 'login'])->middleware('guest');
+
+// Register Routes
 Route::post('register', [RegisterController::class, 'store']);
+
+// Login Routes
+Route::post('login', [LoginController::class, 'authenticate']);
+Route::post('logout', [LoginController::class, 'logout']);
